9716686be03acdca8b62c4e5f7807e1bda987a1d
GolaganaGanesh <starganakb5@gmail.com>
2023-08-29 09:50:27 +0530

    sys_pop

--- wind_pop.bat
+++ wind_pop.bat
@@ -1,24 +1,10 @@
 @echo off
+echo Hostname: %COMPUTERNAME%
+echo User Name: %USERNAME%
+echo Operating System: %OS%
+echo Processor: %PROCESSOR_IDENTIFIER%
+echo Architecture: %PROCESSOR_ARCHITECTURE%
+echo Total Memory: %TOTALPHYSICALMEMORY%
+echo Free Memory: %AVAILPHYSICALMEMORY%
+pause
 
-for /f "delims=" %%a in ('hostname') do set "hostname=%%a"
-set "memory_information="
-for /f "skip=1 tokens=2,6" %%a in ('systeminfo ^| find "Available Physical Memory"') do (
-    set "total_memory=%%a"
-    set "free_memory=%%b"
-    set "memory_information=Total Memory: !total_memory! Free Memory: !free_memory!"
-)
-set "cpu_information="
-for /f "tokens=*" %%a in ('systeminfo') do (
-    set "cpu_information=!cpu_information!%%a"
-)
-set "os_information="
-for /f "usebackq tokens=*" %%a in (`type "C:\Windows\System32\drivers\etc\os-release" 2^>nul`) do (
-    set "os_information=!os_information!%%a"
-)
-
-echo Hostname: %hostname%
-echo Memory Information: %memory_information%
-echo CPU Information:
-echo %cpu_information%
-echo OS Information:
-echo %os_information%
